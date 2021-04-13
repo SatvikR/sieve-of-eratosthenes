9d55fe782206f811be53b3778e9d62bb09422604
Satvik Reddy <reddy.satvik@gmail.com>
2021-04-13 14:38:16 -0700

    removed unnecessary freeing

--- src/primes.c
+++ src/primes.c
@@ -128,10 +128,6 @@ int main(int argc, char **argv)
 		}
 	}
 
-	// This is probably not neccessary but I'm doing it anyway for safety
-	free((char*)print_flag);
-	free((char*)num_flag);
-
 	clock_t t;
 	t = clock();
 
@@ -152,6 +148,7 @@ int main(int argc, char **argv)
 		printf("Press enter to exit...");
 		while (getchar() != '\n');
 	}
+
 	return 0;
 }
 
