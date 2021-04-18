a821dc61c8a3b811e7d64e9898bd06a51edc28f3
Satvik Reddy <reddy.satvik@gmail.com>
2021-04-18 14:46:23 -0700

    switch to mc clang-format

--- src/primes.c
+++ src/primes.c
@@ -18,14 +18,18 @@
 // fast integer ceiling devide
 #define CEIL(x, y) (1 + ((x - 1) / y))
 
-inline void set_bit(uint8_t *bits, int len, int index, bool value) {
+inline void set_bit(uint8_t *bits, int len, int index, bool value)
+{
     assert(index < len);
     uint8_t *byte = &bits[index / 8]; // byte in which the target bit lives
 
-    if (value) {
+    if (value)
+    {
         // set bit to 1
         *byte = *byte | (1 << ((8 * (index / 8 + 1)) - index - 1));
-    } else {
+    }
+    else
+    {
         // This will set the bit to 0
         uint8_t n = 1 << ((8 * (index / 8 + 1)) - index - 1);
         *byte = *byte & (~n);
@@ -33,23 +37,27 @@ inline void set_bit(uint8_t *bits, int len, int index, bool value) {
     return;
 }
 
-inline bool get_bit(uint8_t *bits, int len, int index) {
+inline bool get_bit(uint8_t *bits, int len, int index)
+{
     assert(index < len);
     uint8_t *byte = &bits[index / 8]; // byte in which the target bit lives
 
     return *byte & (1 << ((8 * (index / 8 + 1)) - index - 1));
 }
 
-void print_primes(uint8_t *c, int max) {
+void print_primes(uint8_t *c, int max)
+{
     int bitlen = sizeof(uint8_t) * CEIL(max, 8) * 8;
-    for (int i = 0; i < max; i++) {
+    for (int i = 0; i < max; i++)
+    {
         if (get_bit(c, bitlen, i))
             printf("%d, ", i + 1);
     }
     printf("\n\n");
 }
 
-uint8_t *primes(int max) {
+uint8_t *primes(int max)
+{
     int len = CEIL(max, 8);                 // length in bytes
     int bitlen = sizeof(uint8_t) * len * 8; // Length in bits (rounded up)
     uint8_t *bits = malloc(sizeof(uint8_t) * len);
@@ -76,7 +84,8 @@ uint8_t *primes(int max) {
     return bits;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     const char *print_flag = "--suppress-output";
     const char *num_flag = "--num";
     const char *no_i_flag = "--no-i";
@@ -85,21 +94,33 @@ int main(int argc, char **argv) {
     bool interactive = true;
     int max = DEFAULT_MAX;
 
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            if (strcmp(argv[i], print_flag) == 0) {
+    if (argc > 1)
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            if (strcmp(argv[i], print_flag) == 0)
+            {
                 print = true;
-            } else if (strcmp(argv[i], num_flag) == 0) {
-                if (i >= argc - 1) {
+            }
+            else if (strcmp(argv[i], num_flag) == 0)
+            {
+                if (i >= argc - 1)
+                {
                     printf("Please provide a num with the --num flag");
                     exit(1);
-                } else {
+                }
+                else
+                {
                     max = atoi(argv[i + 1]);
                     i++;
                 }
-            } else if (strcmp(argv[i], no_i_flag) == 0) {
+            }
+            else if (strcmp(argv[i], no_i_flag) == 0)
+            {
                 interactive = false;
-            } else {
+            }
+            else
+            {
                 printf("Argument \"%s\" is unknown\n", argv[i]);
                 exit(1);
             }
@@ -120,7 +141,8 @@ int main(int argc, char **argv) {
 
     printf("primes(%d) took %.4f seconds\n", max, ((float)t) / CLOCKS_PER_SEC);
 
-    if (interactive) {
+    if (interactive)
+    {
         printf("Press enter to exit...");
         while (getchar() != '\n')
             ;
